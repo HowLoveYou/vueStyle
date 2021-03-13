1986775f5b7239096b9fe75aaa533cd5bcfb76b4
machenbo <machenbo@didiglobal.com>
2021-03-13 16:06:28 +0800

    feat:系统搭建

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     // 它支持webPack-dev-server的所有选项
     devServer: {
         port: 8000, // 端口号
-        host: '122.113.5.151',
+        host: 'localhost',
         https: false, // https:{type:Boolean}
         open: true, //配置自动启动浏览器
         // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
