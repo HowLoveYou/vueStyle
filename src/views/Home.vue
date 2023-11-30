b40ea98730a33b98397b137d72450818559f2a1a
machenbo <1330295681@qq.com>
2023-11-30 15:40:59 +0800

    feat:    L

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="home">
-    begin
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import pdf from 'vue-pdf';
 export default {
   name: 'home',
-}
+  components: {
+    pdf,
+  },
+  data() {
+    return {
+      previewUrl:
+        'https://js-dev.sany.com.cn/file-server/cadre/08efc9ddec642367bc9bd638ce373a9c/f94a4c94-899f-4169-a968-7b202c4a05da.pdf',
+      numPages: 0,
+    };
+  },
+  mounted() {
+    let loadingTask = pdf.createLoadingTask(this.previewUrl);
+    loadingTask.promise.then((pdf) => {
+      this.numPages = pdf.numPages;
+    });
+  },
+  methods: {},
+};
 </script>
